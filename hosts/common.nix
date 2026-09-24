e9f64dcd5ec2508604a6a431500880e1e8022f12
gosha20777 <gosha20777@live.ru>
2026-09-24 22:23:04 +0200

    fix(hm): add backupFileExtension and declare editor.fontSize in vscode

--- hosts/common.nix
+++ hosts/common.nix
@@ -30,6 +30,7 @@
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
+    backupFileExtension = "backup";
     extraSpecialArgs = {
       inherit inputs;
       systemSettings = config.systemSettings;
--- modules/home/dev/vscode.nix
+++ modules/home/dev/vscode.nix
@@ -11,6 +11,7 @@ in
     enable = true;
     package = pkgs.vscode;
     profiles.default.userSettings = {
+      "editor.fontSize" = 24;
       "workbench.colorTheme" = "Default Dark Modern";
       "workbench.colorCustomizations" = theme.vscode.colorCustomizations;
       "editor.tokenColorCustomizations" = theme.vscode.tokenColorCustomizations;
