5bc463f957a2b91a5981093e9a6279f4c6d17d87
gosha20777 <gosha20777@live.ru>
2026-09-25 19:27:27 +0200

    fix(network): add nm-openconnect user and ephemeral secret flags for SAML VPN
    - Add users.users.nm-openconnect system user required by D-Bus policies and privilege separation
    - Set cookie-flags and ephemeral secret flags (NM_SETTING_SECRET_FLAG_NOT_SAVED)
    - Update useragent to AnyConnect

--- modules/nixos/core/network.nix
+++ modules/nixos/core/network.nix
@@ -21,16 +21,34 @@
         };
         vpn = {
           service-type = "org.freedesktop.NetworkManager.openconnect";
+          authtype = "password";
+          autoconnect-flags = "0";
+          certsigs-flags = "0";
+          cookie-flags = "2";
+          disable_udp = "no";
+          enable_csd_trojan = "no";
           gateway = "vpngw.uni-wuerzburg.de";
-          usergroup = "Standard (MFA)";
+          gateway-flags = "2";
+          gwcert-flags = "2";
+          lasthost-flags = "0";
+          pem_passphrase_fsid = "no";
+          prevent_invalid_cert = "no";
           protocol = "anyconnect";
+          resolve-flags = "2";
+          stoken_source = "disabled";
           useragent = "AnyConnect";
+          xmlconfig-flags = "0";
         };
       };
     };
   };
 
   programs.nm-applet.enable = true;
+  # NetworkManager-openconnect requires a system user for D-Bus policies and privilege separation
+  users.users.nm-openconnect = {
+    isSystemUser = true;
+    group = "networkmanager";
+  };
 
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
