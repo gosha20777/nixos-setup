723728925af98ed6da4d8692aa1928bde324cc53
gosha20777 <gosha20777@live.ru>
2026-09-25 18:27:51 +0200

    feat(network): configure OpenConnect VPN for Uni-Wuerzburg with SAML auth
    - Enable networkmanager-openconnect plugin for NetworkManager
    - Enable nm-applet as D-Bus Secret Agent for SAML webview dialog
    - Declaratively configure Uni-Wuerzburg VPN profile with required User-Agent

--- modules/nixos/core/network.nix
+++ modules/nixos/core/network.nix
@@ -9,7 +9,28 @@
   # Networking, Bluetooth
   # networking.hostName is set per-host in hosts/<hostname>/default.nix.
   ############################################################
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    plugins = [ pkgs.networkmanager-openconnect ];
+    ensureProfiles.profiles = {
+      Uni-Wuerzburg = {
+        connection = {
+          id = "Uni-Wuerzburg";
+          type = "vpn";
+          autoconnect = false;
+        };
+        vpn = {
+          service-type = "org.freedesktop.NetworkManager.openconnect";
+          gateway = "vpngw.uni-wuerzburg.de";
+          usergroup = "Standard (MFA)";
+          protocol = "anyconnect";
+          useragent = "AnyConnect - OpenConnect";
+        };
+      };
+    };
+  };
+
+  programs.nm-applet.enable = true;
 
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
